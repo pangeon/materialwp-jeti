6ffc7f68f3569069565dec3155321ee61c511e5d
Kamil Cecherz <kamil.cecherz@gmail.com>
2021-11-28 21:02:08 +0100

    add less, include custom css to themes

--- inc/scripts.php
+++ inc/scripts.php
@@ -8,6 +8,8 @@ function materialwp_scripts() {
 
 	wp_enqueue_style( 'materialwp-style', get_stylesheet_directory_uri() . '/style.min.css', array(), '0.0.5' );
 
+	wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/custom.css', array(), '0.0.5' );
+
 	wp_enqueue_script( 'materialwp-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), ' ', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
