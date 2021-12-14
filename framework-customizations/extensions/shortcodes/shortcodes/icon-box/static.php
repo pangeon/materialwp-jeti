2935180de6765d74f38f204a421f4fe1cd3e7f92
Kamil Cecherz <kamil.cecherz@gmail.com>
2021-12-14 20:14:13 +0100

    working with icon-boxes

---
+++ framework-customizations/extensions/shortcodes/shortcodes/icon-box/static.php
@@ -0,0 +1,8 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+$uri = get_template_directory_uri('/extensions/shortcodes/shortcodes/icon-box');
+wp_enqueue_style('fw-shortcode-icon-box-custom', $uri . '/static/css/style.css');
+
+
+
+
