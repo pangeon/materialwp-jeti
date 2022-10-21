17674e796f2ef4f000a4c1b6bce0b429c3c06a9a
Kamil Cecherz <kamil.cecherz@gmail.com>
2022-10-21 23:50:34 +0200

    end working with theme - ver. 1.0

--- js/main.js
+++ js/main.js
@@ -1,47 +1,13 @@
 // @ts-nocheck
-jQuery(document).ready(function() {
-    //addClassWhenScrollPage('.navbar-static', '.navbar-scrolled');
-    styleFixedHeader();
-
+jQuery(document).ready(() => {
+    styleFlickerNavbar('nav.navbar.navbar-expand-lg.navbar-dark.bg-dark', 'navbar-scrolled');
 });
 
-// // @ts-nocheck
-// function addClassWhenScrollPage(className, fixedClassName) {
-//     jQuery(window).scroll(function() {
-//         if (isScrolled(className)) {
-//             jQuery(className).addClass(fixedClassName);
-//         } else {
-//             jQuery(className).removeClass(fixedClassName);
-//         }
-//     });
-// }
-
-// // @ts-nocheck
-// function isScrolled(className) {
-//     let scrolled = jQuery(window).scrollTop > checkHTMLelementPosition('top', className) ? true : false;
-//     return scrolled;  
-// }
-
-// // @ts-nocheck
-// function checkHTMLelementPosition(position, className) {
-//     let elementPosition = 0;
-//     if (position === 'top') {
-//         elementPosition = jQuery(className).offset().top;
-//     } else if(position === 'left') {
-//         elementPosition = jQuery(className).offset().left;
-//     }
-//     return elementPosition
-// }
-
-function styleFixedHeader() {
-	var headerTop = jQuery('nav.navbar.navbar-expand-lg.navbar-dark.bg-dark').offset().top;
-	jQuery(window).scroll(function() {
-		var scrollVal = jQuery(window).scrollTop();
-		if (scrollVal > headerTop) {
-			jQuery('nav.navbar.navbar-expand-lg.navbar-dark.bg-dark').addClass('navbar-scrolled');
-		} else {
-			jQuery('nav.navbar.navbar-expand-lg.navbar-dark.bg-dark').removeClass('navbar-scrolled');
-		}
+function styleFlickerNavbar(sourceStyle, changedStyle) {
+	let headerTop = jQuery(sourceStyle).offset().top;
+	jQuery(window).scroll(() => {
+		let scrollVal = jQuery(window).scrollTop();
+		(scrollVal > headerTop) ? jQuery(sourceStyle).addClass(changedStyle) : jQuery(sourceStyle).removeClass(changedStyle);
 	});
 }
     
